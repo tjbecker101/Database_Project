a4e28dcb834906aceff453995509aab706a6ba7c
Tyler Becker <tjbecker@colossus.it.mtu.edu>
2021-04-28 00:39:46 -0400

    Formatted Change Password

--- changepassword.php
+++ changepassword.php
@@ -57,11 +57,52 @@ try{
 
     
     <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
+    <link href="changepass.css" rel="stylesheet">
   </head>
-  <body class="text-center">
-    
-<main class="form-signin">
+  
+  <body>
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Surveys</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+      <a class="nav-link" href="landingpage.php">Logout</a>
+    </li>
+  </ul>
+</header>
+
+
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="teacherview.php">
+              <span data-feather="home"></span>
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="teachercourseview.php">
+              <span data-feather="circle"></span>
+              View Courses
+            </a>
+          </li>
+		  <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              <span data-feather="key"></span>
+              Change Password
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
   <form method="post" action="changepassword.php">
 	<?php
 		if($default){
