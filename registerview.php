6d78272a50c732c23f6ff2926b8dfbfb2c8e5307
Tyler Becker <tjbecker@colossus.it.mtu.edu>
2021-04-28 20:19:47 -0400

    Changed Webpage Title

--- registerview.php
+++ registerview.php
@@ -17,7 +17,7 @@ if($type == "Instructor"){
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.82.0">
-    <title>Dashboard Template · Bootstrap v5.0</title>
+    <title>Register</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
 
